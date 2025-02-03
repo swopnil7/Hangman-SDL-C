7b4560381b3a201002a575736bcc7fe65e81deb6
swopnil7 <report2swopnil@gmail.com>
2025-02-03 18:56:47 +0545

    minor

--- src/renderer.c
+++ src/renderer.c
@@ -3,10 +3,10 @@
 
 void renderText(SDL_Renderer *renderer, TTF_Font *font, const char *text, int x, int y) {
     if (text == NULL || text[0] == '\0') {
-        return; // Skip rendering if the text is empty
+        return;
     }
 
-    SDL_Color color = {255, 255, 255, 255}; // White color
+    SDL_Color color = {255, 255, 255, 255};
     SDL_Surface *surface = TTF_RenderText_Solid(font, text, color);
     if (!surface) {
         printf("Text rendering failed! TTF_Error: %s\n", TTF_GetError());
